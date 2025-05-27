0af939ba8bec4b9efa84e8f3dd1932cadc0897bc
Ryad Abd <riyadabdessalem18@gmail.com>
2025-05-27 20:55:06 +0100

    basic calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("enter the first number:");
+    let mut number1 = String::new();
+    stdin().read_line(&mut number1).expect("invalid input");
+    println!("enter the second number:");
+    let mut number2 = String::new();
+    stdin().read_line(&mut number2).expect("invalid input");
+    let num1: i32 = number1.trim().parse().expect("error");
+    let num2: i32 = number2.trim().parse().expect("error");
+    println!("the sum of the two nubers is : {}", num1 + num2);
 }
