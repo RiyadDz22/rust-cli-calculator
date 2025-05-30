a6fd47d60e02d3d7512cd8519a6ae2890ba24ddb
Ryad Abd <riyadabdessalem18@gmail.com>
2025-05-30 09:40:43 +0100

    added loop

--- src/main.rs
+++ src/main.rs
@@ -12,33 +12,42 @@ fn str_to_int(num: &str) -> i32 {
 }
 
 fn main() {
-    println!(
-        "Choose the operation you want to perform:
+    loop {
+        println!(
+            "Choose the operation you want to perform:
     1 - Addition (+)
     2 - Subtraction (-)
     3 - Multiplication (*)
-    4 - Division (/)"
-    );
+    4 - Division (/)
+    5 - exit
+    "
+        );
 
-    let operator = take_input("Enter operator (+, -, *, /):");
+        let operator = take_input("Enter operator (1, 2, 3, 4, 5):");
 
-    let number1 = take_input("Enter the first number:");
-    let number2 = take_input("Enter the second number:");
-
-    let num1 = str_to_int(&number1);
-    let num2 = str_to_int(&number2);
-
-    match operator.as_str() {
-        "+" => println!("{num1} + {num2} = {}", num1 + num2),
-        "-" => println!("{num1} - {num2} = {}", num1 - num2),
-        "*" => println!("{num1} * {num2} = {}", num1 * num2),
-        "/" => {
-            if num2 == 0 {
-                println!("Error: Division by zero is not allowed.");
-            } else {
-                println!("{num1} / {num2} = {}", num1 / num2);
-            }
+        if operator == "5" {
+            println!("exiting...bye!");
+            break;
         }
-        _ => println!("Invalid operator."),
+
+        let number1 = take_input("Enter the first number:");
+        let number2 = take_input("Enter the second number:");
+
+        let num1 = str_to_int(&number1);
+        let num2 = str_to_int(&number2);
+
+        match operator.as_str() {
+            "1" => println!("{num1} + {num2} = {}", num1 + num2),
+            "2" => println!("{num1} - {num2} = {}", num1 - num2),
+            "3" => println!("{num1} * {num2} = {}", num1 * num2),
+            "4" => {
+                if num2 == 0 {
+                    println!("Error: Division by zero is not allowed.");
+                } else {
+                    println!("{num1} / {num2} = {}", num1 / num2);
+                }
+            }
+            _ => println!("Invalid operator."),
+        }
     }
 }
